7ca90de80b644f623c8ba9facbc3fe17931ecf5e
Panda <pan.zhang@cyberconnect.me>
2024-06-01 20:13:24 +0800

    iOS widget

---
+++ ios/CyberDuneWidget/CyberDuneWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  CyberDuneWidgetBundle.swift
+//  CyberDuneWidget
+//
+//  Created by spantar on 2024/6/1.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct CyberDuneWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        CyberDuneWidget()
+    }
+}
---
+++ ios/CyberDuneWidget/Extensions.swift
@@ -0,0 +1,11 @@
+//
+//  Extensions.swift
+//  CyberDune
+//
+//  Created by spantar on 2024/6/1.
+//
+
+import Foundation
+import SwiftUI
+import WidgetKit
+
